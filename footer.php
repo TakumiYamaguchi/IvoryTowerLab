d2c70eade06c6f0e7e21d4af5288fbfebf9e9f3d
TakumiYamaguchi <info@ivorytowerlab.jp>
2022-06-16 11:53:39 +0900

    first commit

--- footer.php
+++ footer.php
@@ -11,24 +11,14 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', '_s' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', '_s' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', '_s' ), '_s', '<a href="https://automattic.com/">Automattic</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
+<footer>
+    <div class="footer_inner">
+        <img class="footer_logo" src="<?php echo get_template_directory_uri(); ?>/img/ivory_tower_lab_logo.png" alt="">
+        <p class="Copyright">© 2020 Takumi Yamaguchi</p>
+    </div>
+</footer><!-- #colophon -->
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -12,48 +13,38 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', '_s' ); ?></a>
-
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$_s_description = get_bloginfo( 'description', 'display' );
-			if ( $_s_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $_s_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', '_s' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+    <?php wp_body_open(); ?>
+    <header id="masthead" class="site-header">
+        <div class="header-inner">
+            <h1 class="header-inner__logo">
+                <a href="<?php echo home_url(); ?>/" class="">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/ivory_tower_lab_logo.png" />
+                </a>
+            </h1>
+            <nav class="header-inner__nav">
+                <ul class="header-list__menu">
+                    <li class="header-list-item">
+                        <a class="header-list-item__link" href="">About This Site</a>
+                    </li>
+                    <li class="header-list-item">
+                        <a class="header-list-item__link" href="">Portfolio</a>
+                    </li>
+                    <li class="header-list-item">
+                        <a class="header-list-item__link" href="">My Skill</a>
+                    </li>
+                    <li class="header-list-item">
+                        <a class="header-list-item__link" href="">Contact</a>
+                    </li>
+            </nav>
+        </div>
+    </header>
